275303f1cbae441404c88c3c79ee1c008f9a30f6
Steve Darveau <steve@darvs.net>
2021-12-01 11:51:35 -0500

    - An ugly first part of Day 01 of AoC 2021
    - Solution for Day 01 of AoC 2021

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+    	var prev = Int.MAX_VALUE
+    	return input.map{
+		val bigger = if (it > prev) 1 else 0
+		prev = it
+		bigger
+    	}.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+    	val arr = input.toTypedArray()
+       	return part1((0..(input.size-3)).map{arr[it] + arr[it+1] + arr[it+2]})
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    //val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 666)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map{it.toInt()}
     println(part1(input))
     println(part2(input))
 }
